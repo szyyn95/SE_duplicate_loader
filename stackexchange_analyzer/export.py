67f0fe75f539b0922d5ed78af140e86b2807b45d
Daoyang Shan <szyyn95@gmail.com>
2019-04-05 14:50:27 -0400

    add txt output

--- stackexchange_analyzer/export.py
+++ stackexchange_analyzer/export.py
@@ -1,14 +1,14 @@
 import json
 import csv
 
-def export_to_json(res, export_path):
+def export_to_json(res, export_path, delimiter):
 	"""
 	Not yet implemented
 	"""
 	print ("------NOT YET IMPLEMENTED------")
 	return
 
-def export_to_csv(res, export_path):
+def export_to_csv(res, export_path, delimiter):
 	with open(export_path, 'w', newline='') as csvfile:
 		fieldnames = ['idx', 'q1', 'q2', 'duplicate']
 		writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
@@ -16,3 +16,9 @@ def export_to_csv(res, export_path):
 		writer.writeheader()
 		for curdict in res:
 			writer.writerow(curdict)
+
+def export_to_txt(res, export_path, delimiter):
+	with open(export_path, 'w') as txtfile:
+		txtfile.write(delimiter.join(['idx', 'q1', 'q2', 'duplicate']) + '\n')
+		for curdict in res:
+			txtfile.write(delimiter.join([str(curdict['idx']), curdict['q1'], curdict['q2'], curdict['duplicate']]) + '\n')
